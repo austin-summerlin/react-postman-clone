b33515935115716dd23bd764f021e84163017224
Austin Summerlin <summerlin.austin@gmail.com>
2021-07-30 11:20:06 -0700

    super agent

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+// import AppContainer from '../../containers/AppContainer';
+import Header from '../header/Header';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      {/* <AppContainer /> */}
+    </>);
 }
