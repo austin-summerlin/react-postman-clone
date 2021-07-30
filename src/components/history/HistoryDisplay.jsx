51445ae5be6ecf228f28480ae8e4774697fb3189
Austin Summerlin <summerlin.austin@gmail.com>
2021-07-30 11:19:35 -0700

    history

---
+++ src/components/history/HistoryDisplay.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './history.css';
+
+function HistoryDisplay({ history }) {
+  return (
+    <div className={style.historyContainer}>
+      <div className={style.containerTop}>
+        <p className={style.textContent}>History</p>
+      </div>
+      <ul aria-label="requests">
+        {!!history.length && history.map((item, i) => (
+          <li key={`${i}-${item.method}-${item.url}`}>
+            <span>{item.method}</span>
+            <span>{item.url}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+HistoryDisplay.propTypes = {
+  history: PropTypes.arrayof(PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+  }))
+};
+
+export default HistoryDisplay;
