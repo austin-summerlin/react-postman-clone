2ab893cb0f28d883eb11d58f1148fe337e11d8de
Austin Summerlin <summerlin.austin@gmail.com>
2021-07-30 11:19:53 -0700

    app container

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable max-len */
 import React, { Component } from 'react';
+import Form from '../components/form/Form';
+import ResultsDisplay from '../components/display/ResultsDisplay';
+import HistoryDisplay from '../components/history/HistoryDisplay';
 import { getData, postData, deleteData, updateData } from '../components/services/CrudUtils';
+import style from './container.css';
 
 export default class AppContainer extends Component {
   state = {
@@ -14,24 +18,24 @@ export default class AppContainer extends Component {
 
   handleMethodChange = ({ target }) => {
     this.setState({
-      method: target.value
+      method: target.value,
     });
   };
 
-  hanldeInputChange = ({ target }) => {
-    const { name, value } = target;
-
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
     this.setState({
       [name]: value
     });
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
     this.setState({ loading: true });
     const { method, url, reqBody, history } = this.state;
 
     if (method === 'GET') {
+      const response = await getData(url);
       this.setState((prevState) => ({
         history: [...prevState.history, { method, url }],
         response,
@@ -39,6 +43,7 @@ export default class AppContainer extends Component {
       }));
     }
     else if (method === 'POST') {
+      const response = await postData(url);
       this.setState((prevState) => ({
         history: [...prevState.history, { method, url }],
         response,
@@ -46,6 +51,7 @@ export default class AppContainer extends Component {
       }));
     }
     else if (method === 'DELETE') {
+      const response = await deleteData(url);
       this.setState((prevState) => ({
         history: [...prevState.history, { method, url }],
         response,
@@ -53,24 +59,32 @@ export default class AppContainer extends Component {
       }));
     }
     else if (method === 'PUT') {
+      const response = await updateData(url);
       this.setState((prevState) => ({
         history: [...prevState.history, { method, url }],
         response,
         loading: false,
       }));
     }
-  };
+  }
 
   render() {
-    const { loading, method, url, reqBody, response, history } = this.state;
-    if (loading) return <h1>fetching data...</h1>;
     return (
-      <>
-      </>
-
+      <div className={style.mainContainer}>
+        <HistoryDisplay history={this.state.history} />
+        <div className={style.rightContainer}>
+          <Form
+            handleSumbit={this.handleSubmit}
+            method={this.state.method}
+            onMethodChange={this.handleMethodChange}
+            handleInputChange={this.handleInputChange}
+            url={this.state.url}
+            reqBody={this.state.reqBody} />
+          <ResultsDisplay
+            response={this.state.response}
+            loading={this.state.loading} />
+        </div>
+      </div>
     );
   }
 }
-
-
-
