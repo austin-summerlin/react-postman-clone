e2007dc044cd58b22c3413d416c742b7dd2cb777
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-01 11:51:45 -0700

    put radio in own file

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,95 +1,30 @@
 /* eslint-disable max-len */
 import React from 'react';
 import PropTypes from 'prop-types';
+import RadioButtons from '../radio/Radio';
 import style from '../form/style.css';
 
-const Form = ({ handleSubmit, method, onMethodChange, handleInputChange, url, reqBody }) => {
+const Form = ({ url, body, method, onSubmit, onChange }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          <input
-            placeholder="enter a valid url"
-            className={style.form}
-            type="text"
-            name="url"
-            onChange={handleInputChange}
-            value={url} />
-        </label>
-        <button
-          className={style.submitButton}
-          aria-label="fetch-button">
-          submit
-        </button>
-      </div>
-
-      <div className={style.radioContainer}>
-        <label className={style.radioLabel}>
-          <input
-            data-testid="get-button"
-            type="radio"
-            name="method"
-            value="GET"
-            checked={method === 'GET'}
-            onChange={onMethodChange} />
-          <span>get</span>
-        </label>
-
-        <label className={style.radioLabel}>
-          <input
-            data-testid="post-button"
-            type="radio"
-            name="method"
-            value="POST"
-            checked={method === 'POST'}
-            onChange={onMethodChange} />
-          <span>post</span>
-        </label>
-
-        <label className={style.radioLabel}>
-          <input
-            data-testid="put-button"
-            type="radio"
-            name="method"
-            value="PUT"
-            checked={method === 'PUT'}
-            onChange={onMethodChange} />
-          <span>put</span>
-        </label>
-
-        <label className={style.radioLabel}>
-          <input
-            data-testid="delete-button"
-            type="radio"
-            name="method"
-            value="DELETE"
-            checked={method === 'DELETE'}
-            onChange={onMethodChange} />
-          <span>delete</span>
-        </label>
-      </div>
-
-      <div className={style.reqBody}>
-        <label>
-          <textarea
-            placeholder="Raw JSON"
-            type="text"
-            className={style.reqBody}
-            name="reqBody"
-            onChange={handleInputChange}
-            value={reqBody} />
-        </label>
-      </div>
-    </form>
+    <section className={style.form}>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="url" placeholder="URL" value={url} onChange={onChange} />
+        <section>
+          <RadioButtons method={method} onChange={onChange} />
+          <button>Go</button>
+        </section>
+        <textarea name="body" placeholder="Raw JSON Body" value={body} onChange={onChange} />
+      </form>
+    </section>
   );
 };
+
 Form.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
   method: PropTypes.string.isRequired,
-  onMethodChange: PropTypes.func.isRequired,
-  handleInputChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
   url: PropTypes.string.isRequired,
-  reqBody: PropTypes.any.isRequired,
+  body: PropTypes.any.isRequired,
 };
 
 export default Form;
