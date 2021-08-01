31a46c1480353c60d25b190d1584706fc2a5006a
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-01 11:51:58 -0700

    alter history

--- src/components/history/HistoryDisplay.jsx
+++ src/components/history/HistoryDisplay.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import style from './history.css';
 
-function HistoryDisplay({ history }) {
 
+const HistoryDisplay = ({ method, url, id }) => {
   return (
-    <div className={style.historyContainer}>
-      <div className={style.containerTop}>
-        <p className={style.textContent}>previous...</p>
-      </div>
-      <ul aria-label="requests">
-        {!!history.length && history.map((item, i) => (
-          <li key={`${i}-${item.method}-${item.url}`}>
-            <span>{item.method} </span>
-            <span>{item.url}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <article id={id}>
+      <p id={id}>{method}</p>
+      <span id={id}>{url}</span>
+    </article>
   );
-}
+};
+
 
 HistoryDisplay.propTypes = {
-  history: PropTypes.arrayOf(PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    method: PropTypes.string.isRequired,
-  }))
+  id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
 };
 
 export default HistoryDisplay;
